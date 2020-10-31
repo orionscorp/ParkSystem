264b54e161ae9b63f10ae7497ba407484a665040
orionscorp <73734694+orionscorp@users.noreply.github.com>
2020-10-31 14:59:11 +0800

    Update models.py

--- member/models.py
+++ member/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000)
+    isbn = models.CharField('ISBN', max_length=13)
+    genre = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        return self.title
+
+    def getGenres(self):
+        genreListString = ', '.join(map(str,[item.name for item in self.genre.all()]))
+        return genreListString
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
